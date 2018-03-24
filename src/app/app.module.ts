4a70da566b1e36291bfe320afe92a6d9fc4e78da
Ryan Archer <ryanarcher@Ryans-MacBook-Pro.local>
2018-03-24 21:16:30 +1000

    Add pages to app module ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { FilterPage } from '../pages/filter/filter';
+import { MacroDetailPage } from '../pages/macro-detail/macro-detail';
+import { ContributorsPage } from '../pages/contributors/contributors';
+import { GlossaryPage } from '../pages/glossary/glossary';
+import { LocatorPage } from '../pages/locator/locator';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
